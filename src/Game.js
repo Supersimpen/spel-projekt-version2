c0f145204777d119ab36b4c9d607dcb3b838eba9
Supersimpen <magnus.glas@elev.ga.ntig.se>
2025-01-27 11:12:19 +0100

    Attack

--- src/Game.js
+++ src/Game.js
@@ -41,6 +41,31 @@ export default class Game {
       this.enemies.push(enemy); 
       this.lastSpawnTime = 0; 
     }
+    document.addEventListener('keydown', function(event) {
+      if (event.code === 'Space') {
+          attack();
+      }
+  });
+  function attack() {
+    const attackArea = {
+        x: player.x + player.width, 
+        y: player.y,
+        width: 50, 
+        height: player.height 
+    };
+
+    enemies.forEach((enemy, index) => {
+        if (isColliding(attackArea, enemy)) {
+            enemies.splice(index, 1);
+        }
+    });
+}
+function isColliding(rect1, rect2) {
+  return rect1.x < rect2.x + rect2.width &&
+         rect1.x + rect1.width > rect2.x &&
+         rect1.y < rect2.y + rect2.height &&
+         rect1.y + rect1.height > rect2.y;
+}
 
 
     for (const enemy of this.enemies) {
